a754f8535c6fc8040886b7f5d4ca46e45f7fcd8e
BiggieLittles <156357409+BiggieLittles@users.noreply.github.com>
2024-06-02 00:40:48 -0400

    fixed functionality

--- index.js
+++ index.js
@@ -16,3 +16,33 @@ const db = mysql.createConnection({
 }
 
 displayDept();
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "menu",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+      ],
+    },
+  ])
+  .then((answer) => {
+    switch (answer.menu) {
+      case "View all departments":
+        displayDept();
+        break;
+    } 
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+  
